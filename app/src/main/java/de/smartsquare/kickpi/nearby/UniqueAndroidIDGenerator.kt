7e3d454ed66a3eef68e7a2feb3fb2b841796e81d
darivs <dtack192@gmail.com>
2018-08-23 09:56:16 +0200

    add UniqueIDGenerator
    closes #1

--- app/src/main/java/de/smartsquare/kickpi/nearby/UniqueAndroidIDGenerator.kt
+++ app/src/main/java/de/smartsquare/kickpi/nearby/UniqueAndroidIDGenerator.kt
@@ -1,1 +1,11 @@
 package de.smartsquare.kickpi.nearby
+
+import android.annotation.SuppressLint
+import android.app.Activity
+import android.provider.Settings
+
+class UniqueAndroidIDGenerator(private val activity : Activity) : IDGenerator {
+
+    @SuppressLint("HardwareIds")
+    override fun generate(): String = Settings.Secure.getString(activity.applicationContext.contentResolver, Settings.Secure.ANDROID_ID)
+}
