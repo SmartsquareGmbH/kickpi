4fbd57e6e0af9b8f5283780269b160b32991c805
darivs <dtack192@gmail.com>
2018-08-23 16:09:20 +0200

    Add confetto

--- app/src/main/java/de/smartsquare/kickpi/MainActivity.kt
+++ app/src/main/java/de/smartsquare/kickpi/MainActivity.kt
@@ -1,18 +1,21 @@
 package de.smartsquare.kickpi
 
 import android.app.Activity
+import android.graphics.Color
 import android.os.Bundle
-import com.google.android.things.pio.PeripheralManager
 import de.smartsquare.kickpi.gpio.GPIOManager
 import de.smartsquare.kickpi.http.HTTPManager
 import de.smartsquare.kickpi.ioc.ActivityModule
 import de.smartsquare.kickpi.ioc.DaggerContainer
 import de.smartsquare.kickpi.nearby.NearbyManager
+import kotlinx.android.synthetic.main.activity_main.*
+import nl.dionsegijn.konfetti.KonfettiView
+import nl.dionsegijn.konfetti.models.Shape
+import nl.dionsegijn.konfetti.models.Size
 import org.greenrobot.eventbus.EventBus
 import javax.inject.Inject
 
 class MainActivity : Activity() {
-
 
     @Inject
     lateinit var gpioManager: GPIOManager
@@ -33,5 +36,22 @@ class MainActivity : Activity() {
         EventBus.getDefault().register(nearbyManager)
         EventBus.getDefault().register(httpManager)
         EventBus.getDefault().post(StartIdleEvent())
+
+        confetto()
+    }
+
+    fun confetto(){
+        val confettiContainer = findViewById<KonfettiView>(R.id.viewKonfetti)
+
+        confettiContainer.build()
+                .addColors(Color.RED, Color.argb(0, 204, 0, 51), Color.argb(0, 204, 0, 0))
+                .setDirection(0.0, 350.0)
+                .setSpeed(3f, 8f)
+                .setFadeOutEnabled(true)
+                .setTimeToLive(10000)
+                .addShapes(Shape.RECT, Shape.CIRCLE)
+                .addSizes(Size(10))
+                .setPosition(viewKonfetti.width - 0f, viewKonfetti.height - 0f)
+                .stream(100, 5000L)
     }
 }
