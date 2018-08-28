c34137800eef34098b9c9abf46f911d3526beb3e
Dennis Dierkes <dierkes@smartsquare.de>
2018-08-28 15:03:27 +0200

    Block lobby creation while game is in progress

--- app/src/main/kotlin/de/smartsquare/kickpi/create/CreateLobbyUseCase.kt
+++ app/src/main/kotlin/de/smartsquare/kickpi/create/CreateLobbyUseCase.kt
@@ -6,8 +6,13 @@ import com.google.android.gms.nearby.messages.MessageListener
 import com.google.android.gms.nearby.messages.MessagesClient
 import de.smartsquare.kickpi.AuthorizationService
 import de.smartsquare.kickpi.Lobby
+import de.smartsquare.kickpi.MatchInProgressException
 import de.smartsquare.kickpi.NearbyAdapter
 import de.smartsquare.kickpi.NearbyAdapter.Companion.fromNearby
+import de.smartsquare.kickpi.join.NewPlayerJoinedEvent
+import de.smartsquare.kickpi.leave.PlayerLeavedEvent
+import de.smartsquare.kickpi.play.score.GoalScoredEvent
+import de.smartsquare.kickpi.play.start.GameStartedEvent
 import de.smartsquare.kickpi.throwIllegalArgumentExceptionIfBlank
 import org.greenrobot.eventbus.EventBus
 import javax.inject.Inject
@@ -21,6 +26,7 @@ class CreateLobbyUseCase @Inject constructor(
     private val TAG = "Create Lobby"
 
     override fun onFound(message: Message) {
+        if (eventBus.isGameInProgress()) throw MatchInProgressException()
         val createLobbyMessage = fromNearby(message, CreateLobbyMessage::class.java)
 
         with(createLobbyMessage) {
@@ -37,4 +43,16 @@ class CreateLobbyUseCase @Inject constructor(
         messagesClient.publish(NearbyAdapter.toNearby(inLobbyCreationBroadcast, "LOBBY_CREATION"))
         Log.i(TAG, "Broadcast in lobby creation state")
     }
+
+    @Suppress("NOTHING_TO_INLINE")
+    private inline fun EventBus.isGameInProgress() =
+        listOf(
+            NewPlayerJoinedEvent::class.java,
+            LobbyCreatedEvent::class.java,
+            PlayerLeavedEvent::class.java,
+            GameStartedEvent::class.java,
+            GoalScoredEvent::class.java
+        ).map { this.getStickyEvent(it) }
+            .any { it != null }
+
 }
--- app/src/test/kotlin/de/smartsquare/kickpi/create/CreateLobbyUseCaseSpecification.kt
+++ app/src/test/kotlin/de/smartsquare/kickpi/create/CreateLobbyUseCaseSpecification.kt
@@ -2,19 +2,28 @@ package de.smartsquare.kickpi.create
 
 import com.google.android.gms.nearby.messages.MessagesClient
 import de.smartsquare.kickpi.AuthorizationService
+import de.smartsquare.kickpi.Lobby
+import de.smartsquare.kickpi.MatchInProgressException
 import de.smartsquare.kickpi.NearbyAdapter
+import de.smartsquare.kickpi.play.score.GoalScoredEvent
 import io.mockk.mockk
 import io.mockk.verify
 import org.amshove.kluent.shouldBeEqualTo
+import org.amshove.kluent.shouldThrow
 import org.greenrobot.eventbus.EventBus
-import org.junit.Test
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.Test
 
 class CreateLobbyUseCaseSpecification {
 
     private val eventBus = EventBus.getDefault()
-    private val authorizationService = mockk<AuthorizationService>(relaxed = true)
     private val messagesClient = mockk<MessagesClient>(relaxed = true)
+    private val authorizationService = mockk<AuthorizationService>(relaxed = true)
     private val createLobbyUseCase = CreateLobbyUseCase(authorizationService, eventBus, messagesClient)
+
+    @AfterEach fun cleanupEventBus() {
+        eventBus.removeAllStickyEvents()
+    }
 
     @Test fun `publish sticky LobbyCreatedEvent if authorization was successful`() {
         val createLobbyMessage = CreateLobbyMessage("deen", "1337")
@@ -29,5 +38,15 @@ class CreateLobbyUseCaseSpecification {
         createLobbyUseCase.onFound(NearbyAdapter.toNearby(createLobbyMessage, "CREATE_LOBBY"))
 
         verify { messagesClient.publish(any()) }
+    }
+
+    @Test fun `create lobby while a game is in process`() {
+        eventBus.postSticky(GoalScoredEvent(Lobby("deen")))
+
+        val createLobbyMessage = CreateLobbyMessage("deen", "1337")
+        val nearbyCreateLobbyMessage = NearbyAdapter.toNearby(createLobbyMessage, "CREATE_LOBBY")
+        val lobbyCreation = { createLobbyUseCase.onFound(nearbyCreateLobbyMessage) }
+
+        lobbyCreation shouldThrow MatchInProgressException::class
     }
 }
