6f17445a5be8cb622379a7073addcb3d6e20240a
Dennis Dierkes <dierkes@smartsquare.de>
2018-09-17 14:18:15 +0200

    Add error handling in game repository
    fixes #8
    fixes #10

--- app/src/main/kotlin/de/smartsquare/kickpi/playing/GameRepository.kt
+++ app/src/main/kotlin/de/smartsquare/kickpi/playing/GameRepository.kt
@@ -1,13 +1,21 @@
 package de.smartsquare.kickpi.playing
 
+import android.util.Log
 import de.smartsquare.kickpi.domain.LobbyViewModel
 import de.smartsquare.kickpi.playing.Game.Team
+import io.reactivex.Completable
+import io.reactivex.Observable
+import io.reactivex.Single
+import io.reactivex.schedulers.Schedulers
 import okhttp3.ResponseBody
 import retrofit2.Call
 import retrofit2.http.Body
 import retrofit2.http.POST
+import java.util.concurrent.TimeUnit
 
 class GameRepository(private val kickchain: KickchainGameRepository) {
+
+    private val TAG = "KICKPI"
 
     fun save(lobby: LobbyViewModel) {
         lobby.let {
@@ -16,14 +24,24 @@ class GameRepository(private val kickchain: KickchainGameRepository) {
             val score = Game.Score(lobby.scoreLeft.value, lobby.scoreRight.value)
 
             Game(leftTeam, rightTeam, score)
-        }.also { kickchain.save(it).execute() }
+        }.also { game ->
+            kickchain.save(game)
+                .subscribeOn(Schedulers.io())
+                .retryWhen { request ->
+                    request.delay(5, TimeUnit.SECONDS)
+                        .also { Log.i(TAG, "Error while storing [$game]. Retrying every 5 seconds.") }
+                }
+                .subscribe {
+                    Log.i(TAG, "Successfully stored $game.")
+                }
+        }
     }
 }
 
 interface KickchainGameRepository {
 
     @POST("game")
-    fun save(@Body game: Game): Call<ResponseBody>
+    fun save(@Body game: Game): Completable
 }
 
 data class Game(val team1: Team, val team2: Team, val score: Score) {
